4b222c1d76d69413babc0bb71c751c1c56abc3b2
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2022-01-26 20:08:48 +0200

    test actions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-	return <div className="App">My site</div>;
+	return <div className="App">My site is ready</div>;
 }
 
 export default App;
