75bdbfd10e2006fe088beb84bce04b77bbbaf9a8
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2022-05-29 16:37:31 +0300

    Initial set up for calls

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CanvasView from "./components/CanvasView";
 import NavBar from "./components/navbar/NavBar";
 
 import "./styles/App.scss";
+import axios from "axios";
 
 const defaultJourneyStep = {
 	key: "Home",
@@ -151,6 +152,10 @@ function App() {
 		const lastViewedStep = localStorage.getItem("lastViewedStep");
 		if (lastViewedStep) dispatchJourneyStep({ type: lastViewedStep, payload: isScreenMobile });
 	}, [isScreenMobile]);
+
+	useEffect(() => {
+		axios.get("/api/get_all");
+	}, []);
 
 	return (
 		<JourneyStepsContext.Provider value={{ journeyStep, dispatchJourneyStep }}>
